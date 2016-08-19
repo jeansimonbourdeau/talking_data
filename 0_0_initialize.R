4373369896420bfa6f434d7d4947b98fe50f3e6b
gwielinski <g.wielinski@hotmail.ca>
2016-08-19 13:22:01 -0400

    Delete 0_0_initialize.R

--- 0_0_initialize.R
+++
@@ -1,6 +0,0 @@
-#Importer les packages de base
-library(dplyr)			#Pour la manipulation des données
-library(tidyr)			#Pour la manipulation des données
-library(ggplot2)		#Pour effectuer des graphiques
-library(lubridate)		#Pour travailler les dates
-library(xda)			#Pour l'exploration descriptive des données https://www.r-bloggers.com/introducing-xda-r-package-for-exploratory-data-analysis/
