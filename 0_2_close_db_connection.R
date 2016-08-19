d05e648dcac2dacebc27cc6b0d6c6d612515471a
gwielinski <g.wielinski@hotmail.ca>
2016-08-19 13:23:02 -0400

    Delete 0_2_close_db_connection.R

--- 0_2_close_db_connection.R
+++
@@ -1,7 +0,0 @@
-###Ce script permet de fermer la connection à la base de données sous postgreSQL###
-#######
-dbDisconnect(con)
-# Detach Package
-detach("package:RPostgreSQL", unload=TRUE)
-##	Fin Import
-#######
